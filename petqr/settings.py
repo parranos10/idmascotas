20ba15ecb845e55b3cca54de1070cb3dbadbce2e
parri <parranos10@gmail.com>
2026-05-25 05:16:07 -0500

    fix static cloudi

--- petqr/settings.py
+++ petqr/settings.py
@@ -120,7 +120,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
