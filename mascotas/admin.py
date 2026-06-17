71bd80cf0b1562ebff52024ad75c6e48b1b26d8b
parri <parranos10@gmail.com>
2026-06-17 16:12:27 -0500

    agregar boton qr

--- mascotas/admin.py
+++ mascotas/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Mascota
 
-admin.site.register(Mascota)
+
+@admin.register(Mascota)
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+        'duenio',
+        'telefono',
+        'raza',
+        'edad',
+        'ver_qr',
+    )
+
+    search_fields = (
+        'nombre',
+        'duenio',
+        'telefono',
+    )
+
+    list_filter = (
+        'raza',
+    )
+
+    def ver_qr(self, obj):
+        url = reverse('perfil_mascota', args=[obj.id])
+        return format_html(
+            '<a class="button" href="{}" target="_blank">Ver QR</a>',
+            url
+        )
+
+    ver_qr.short_description = "QR"
